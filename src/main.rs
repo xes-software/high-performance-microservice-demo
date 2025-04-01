0c85afb635fe52708c57788ac029c9a3d8d052af
leif <leif@elliottnet.com>
2025-04-01 11:58:50 -0400

    working todolist grpc server

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,37 @@
-fn main() {
-    println!("Hello, world!");
+use todo_list::todo_list_server::{TodoList, TodoListServer};
+use todo_list::{AddRequest, Empty, ListResponse};
+use tonic::{Request, Response, Result, Status, transport::Server};
+pub mod todo_list {
+    tonic::include_proto!("todo");
+}
+
+#[derive(Debug, Default)]
+pub struct TodoListService {}
+
+#[tonic::async_trait]
+impl TodoList for TodoListService {
+    async fn add(&self, request: Request<AddRequest>) -> Result<Response<Empty>, Status> {
+        println!("Got a request: {:?}", request);
+        Ok(Response::new(Empty {}))
+    }
+
+    async fn list(&self, request: Request<Empty>) -> Result<Response<ListResponse>, Status> {
+        println!("Got a request: {:?}", request);
+        Ok(Response::new(ListResponse {
+            list: vec!["hello".to_string()],
+        }))
+    }
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let addr = "[::1]:50051".parse()?;
+    let todo_list = TodoListService::default();
+
+    Server::builder()
+        .add_service(TodoListServer::new(todo_list))
+        .serve(addr)
+        .await?;
+
+    Ok(())
 }
